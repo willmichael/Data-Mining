a36fef3c69d7462ad8779525c5447d406abd06fe
willmichael <oleemir@gmail.com>
2017-04-18 16:16:35 -0700

    Part 2

--- project2/main.py
+++ project2/main.py
@@ -1,54 +1,119 @@
+from __future__ import division
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 # TODO: input,work, output
 # TODO: ^ make a more descriptive todo
+ITERATIONS = 10
+LEARNINGRATE = 0.0000001
+
 
 def main():
     (testData_x, testData_y, trainData_x, trainData_y) = importing_data()
-    part_one(testData_x, testData_y, trainData_x, trainData_y)
+    # part_one(testData_x, testData_y, trainData_x, trainData_y)
+    part_two(testData_x, testData_y, trainData_x, trainData_y)
 
 
 
 def part_one(testData_x, testData_y, trainData_x, trainData_y):
     print "part one"
 
+    learningRate = 0.0000001
     #testing regression
-    w = batchLogisticRegression(testData_x[380:420,:], testData_y[380:420])
+    w = batchLogisticRegression(trainData_x, trainData_y,
+                                learningRate)
     #print number of correctly predicted values
     correct = 0
-    for i in range(80):
-        correct = correct + testLogisticRegression(testData_x[380+i,:], testData_y[380+i], w)
-    print correct
+    for i in range(testData_x.shape[0]):
+        correct = correct + testLogisticRegression(testData_x[i,:], testData_y[i], w)
+
+    percentageCorrect = correct/testData_x.shape[0]
+    print percentageCorrect
+
+def part_two(testData_x, testData_y, trainData_x, trainData_y):
+    print "part two"
+
+    learningRate = 0.0000001
+    #testing regression
+    accuracy = batchLogisticRegressionAccuracy(trainData_x, trainData_y,
+                                learningRate, testData_x, testData_y)
+
+
+    # Graph the percentages
+    # graph_sse_x(accuracy[0], accuracy[1], np.arange(ITERATIONS), "percentage",
+                # "iterations")
+
 
 # create w 
-def batchLogisticRegression(x, y):
-    learningRate = .05
+# TODO: Fix Overflow
+def batchLogisticRegression(x, y, lr):
     numFeatures = x.shape[1]
     w = np.zeros(numFeatures)
-
     j = 0
-
     while(1):
         d = np.zeros(numFeatures) 
-
         for i, yi in enumerate(y):
             wxi = np.dot(w, x[i,:])
-
             nwxi = wxi * -1
             denom = 1 + np.exp(nwxi)
             yihat = 1/denom
             error = yi - yihat
-
             d = d + np.multiply(error, x[i,:])
-
-        w = w + np.multiply(learningRate, d)
+        w = w + np.multiply(lr, d)
         j = j + 1
-        if(j > 100):
+        # Break after j iterations
+        if(j >= ITERATIONS):
             break;
-
-    # print w
     return w
+
+def batchLogisticRegressionAccuracy(x, y, lr, x2, y2):
+    numFeatures = x.shape[1]
+    resultsTrain = [] 
+    resultsTest = [] 
+    w = np.zeros(numFeatures)
+    j = 0
+    while(1):
+        d = np.zeros(numFeatures) 
+        for i, yi in enumerate(y):
+            denom = 1 + np.exp(wxi * -1)
+            yihat = 1/denom
+            error = yi - yihat
+            d = d + np.multiply(error, x[i,:])
+        w = w + np.multiply(lr, d)
+        j = j + 1
+
+        (rTrain, rTest) = testAndRecord(x, y, x2, y2, w)
+        resultsTrain.append(rTrain)
+        resultsTest.append(rTest)
+
+        # Stop after j iterations
+        if(j >= ITERATIONS):
+            break;
+    return (resultsTrain, resultsTest)
+
+def testAndRecord(x, y, x2, y2, w):
+    #training accuracy
+    correct = 0
+    for i in range(x.shape[0]):
+        correct = correct + testLogisticRegression(x[i,:], y[i], w)
+
+    percentageCorrect = correct/x.shape[0]
+    # print "training accuracy"
+    # print percentageCorrect
+
+    #testing accuracy
+    correct2 = 0
+    for i in range(x2.shape[0]):
+        correct2 = correct2 + testLogisticRegression(x2[i,:], y2[i], w)
+
+    percentageCorrect2 = correct2/x2.shape[0]
+    # print "testing accuracy"
+    # print percentageCorrect2
+
+    return (percentageCorrect, percentageCorrect2)
+
 
 # returns 1 if analysis was correct and 0 if analysis was incorrect
 def testLogisticRegression(x, y, w):
@@ -66,10 +131,18 @@ def testLogisticRegression(x, y, w):
         if(y == 0):
             return 1
     return -1
-    # print trainData_x
-    print len(trainData_x)
-    print len(trainData_y)
-    # np.savetxt('foo.csv', trainData_x, fmt='%10.5f', delimiter=',')
+
+
+# def graph_sse_x(y, y1, x, ylabel, xlabel, ylegend, y1legend):
+def graph_sse_x(y, y1, x, ylabel, xlabel):
+    line1 = plt.plot(x, y)
+    line2 = plt.plot(x, y1)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+
+    # plt.legend([ylegend,y1legend])
+
+    plt.show()
 
 # TODO: Test if this really parses data correctly
 # TODO: (Possibly) creates a view of original, when view is modified so is
