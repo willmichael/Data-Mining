42c691e281519d3e0f6c3b51f42244e014436230
alxngyn <alexlnguyen@gmail.com>
2017-04-25 00:06:55 -0700

    first commit for project3

---
+++ project3/main.py
@@ -0,0 +1,16 @@
+from __future__ import division
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# TODO: input,work, output
+# TODO: ^ make a more descriptive todo
+
+
+def main():
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
