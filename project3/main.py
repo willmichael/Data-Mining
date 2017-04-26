d873edb823dc009c53939bd5aa7378258c6fe0ec
alxngyn <alexlnguyen@gmail.com>
2017-04-25 20:19:27 -0700

    finished knn algo

--- project3/main.py
+++ project3/main.py
@@ -2,26 +2,65 @@ from __future__ import division
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import math
 
 # TODO: input,work, output
 # TODO: ^ make a more descriptive todo
 
 
 def main():
-    (testData_x, testData_y, trainData_x, trainData_y) = importing_data()
-    print "test data: "
-    print "x"
-    print testData_x
-    print "y"
-    print testData_y
-    "training data:"
-    print "x"
-    print trainData_x
-    print "y"
-    print trainData_y
+    (test_data, train_data) = importing_data()
+    part_one(test_data, train_data)
 
 
+def part_one(test_data, train_data):
+    """
+    Model selection for KNN
+    """
+    k = 5
+    knn_algo(test_data, train_data, k)
 
+
+def part_two():
+    """
+    Decision tree
+    """
+    pass
+
+def part_three():
+    """
+    extra credit
+    """
+    pass
+
+def knn_algo(test_data, train_data, k):
+    # for each test item, find the distance between it and every item in the
+    # training set
+    for test_instance in test_data:
+        distances = []
+        for x in range(len(test_instance)):
+            dist = euclideanDistance(test_instance, train_data[x])
+            distances.append( (train_data[x], dist) )
+        # print distances
+        distances.sort(key=lambda tup: tup[1])
+
+        # once all the distances have been found, find the K nearest neighbors
+        neighbors = []
+        for i in range(k):
+            # store and print the neighbor's class
+            neighbors.append(distances[i][0][0])
+        print neighbors
+
+def euclideanDistance(instance1, instance2):
+    # find length of a instance
+    length = len(instance1)
+    # find distance between two instances
+    distance = 0
+    for x in range(length):
+        distance += pow((instance1[x] - instance2[x]), 2)
+    # print "distance: "
+    # print distance
+    return np.sqrt(distance)
 
 def importing_data():
     print "Importing Data"
@@ -37,12 +76,10 @@ def importing_data():
     (_, testSize_col) = dfTestArr.shape
     (_, trainSize_col) = dfTrainArr.shape
 
-    dfTest_x = dfTestArr[:,1:]
-    dfTest_y = dfTestArr[:,0]
-    dfTrain_x = dfTrainArr[:,1:]
-    dfTrain_y = dfTrainArr[:,0]
+    dfTest = dfTestArr[:,:]
+    dfTrain = dfTrainArr[:,:]
 
-    return (dfTest_x, dfTest_y, dfTrain_x, dfTrain_y)
+    return (dfTest, dfTrain)
 
 if __name__ == '__main__':
     main()
