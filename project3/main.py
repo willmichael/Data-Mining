c125c1097ba0b8d51bc8dda3d69c3e584efeb499
alxngyn <alexlnguyen@gmail.com>
2017-04-25 18:46:34 -0700

    finished importing data

--- project3/main.py
+++ project3/main.py
@@ -8,9 +8,41 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    pass
+    (testData_x, testData_y, trainData_x, trainData_y) = importing_data()
+    print "test data: "
+    print "x"
+    print testData_x
+    print "y"
+    print testData_y
+    "training data:"
+    print "x"
+    print trainData_x
+    print "y"
+    print trainData_y
 
 
+
+
+def importing_data():
+    print "Importing Data"
+    testLoc = './knn_test.csv'
+    trainLoc = './knn_train.csv'
+
+    dfTest = pd.read_csv(testLoc, header=None)
+    dfTrain = pd.read_csv(trainLoc, header=None)
+
+    dfTestArr = dfTest.values
+    dfTrainArr = dfTrain.values
+
+    (_, testSize_col) = dfTestArr.shape
+    (_, trainSize_col) = dfTrainArr.shape
+
+    dfTest_x = dfTestArr[:,1:]
+    dfTest_y = dfTestArr[:,0]
+    dfTrain_x = dfTrainArr[:,1:]
+    dfTrain_y = dfTrainArr[:,0]
+
+    return (dfTest_x, dfTest_y, dfTrain_x, dfTrain_y)
 
 if __name__ == '__main__':
     main()
