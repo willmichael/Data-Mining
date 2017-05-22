c9e6074520e2add443781d108cbdd26230795b47
alxngyn <alexlnguyen@gmail.com>
2017-05-21 17:28:56 -0700

    fixed import and did function outline

--- project4/main.py
+++ project4/main.py
@@ -11,17 +11,33 @@ import math
 
 def main():
     print "hi"
+    data = importing_data()
+    print len(data)
+
+def part_2_1():
+    # implement k means with k = 2
+    pass
+
+def part_2_2():
+    # implement k means with k = range(2,11)
+    pass
+
+def part_3_1():
+    # Implement HAC algorithm using single link to measure the distance
+    # between clusters
+    pass
+
+def part_3_2():
+    # Implement HAC algorithm using complete link to measure the distance
+    # between clusters.
+    pass
+
 
 def importing_data():
     print "Importing Data"
     data = 'data-1.txt'
-
     data = pd.read_csv(data, header=None)
-    dfTrain = pd.read_csv(trainLoc, header=None)
-
-
     dfData = data.values[:,:]
-
     return dfData
 
 
